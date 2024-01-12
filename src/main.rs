ea9ea8467399aecf89fdd59a7e5d8d24b24736af
Quinn-3 <lin.angelina2169@gmail.com>
2024-01-12 09:15:18 -0500

    terrain generation

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ mod neural_net;
 
 #[macroquad::main("Vector Racing")]
 async fn main() {
+    let map = new_map(100, 100);
+
     loop {
         clear_background(BLACK);
+        draw_map(&map, 4.0);
+        next_frame().await
     }
 }
 
@@ -37,6 +41,14 @@ fn new_map(height: usize, width: usize) -> Map {
     m
 }
 
+fn set_pixel(map: &mut Map,x: usize, y: usize, tile: Tile) {
+    map.tiles[y * map.width + x] = tile;
+}
+
+fn get_pixel(map: &Map,x: usize, y: usize) -> Tile {
+    map.tiles[y * map.width + x]
+}
+
 fn generate_map(map: &mut Map) {
     //Step 1: Generate Random Points
     let margin = 10_usize;
@@ -55,16 +67,51 @@ fn generate_map(map: &mut Map) {
             random_points.push((x, y));
         }
     }
-    let mut distances: Vec<f32> = vec![];
-    for (x1, y1) in random_points.iter() {
-        for (x2, y2) in random_points.iter() {
-            distances.push((((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)) as f32).sqrt())
+    let mut visited = vec![false; random_points.len()];
+    let mut ordered_points: Vec<(usize, usize)> = vec![];
+    let (mut curr_x, mut curr_y) = random_points[0];
+    visited[0] = true; //set the first point to visited
+    ordered_points.push((curr_x, curr_y));
+    for _ in 0..N*N {
+        //do a linear search through all the remaining points to find the shortest distance
+        let (mut shortest, mut shortest_idx) = (f32::INFINITY, 0);
+        for (i, (next_x, next_y)) in random_points.iter().enumerate() {
+            if visited[i] {continue;}
+            let (dx, dy) = (curr_x as f32 - *next_x as f32, curr_y as f32 - *next_y as f32);
+            let dist = (dx*dx + dy*dy).sqrt();
+            if dist < shortest {
+                shortest = dist;
+                shortest_idx = i;
+            }
         }
+        ordered_points.push(random_points[shortest_idx]);
+        (curr_x, curr_y) = random_points[shortest_idx];
+        visited[shortest_idx] = true;
     }
-    let visited = vec![false; random_points.len()];
-    let curr_point = random_points[0];
-    for _ in 0..random_points.len() {
+    //testing set all points in ordered points to walls
+    for ((x1, y1),(x2, y2)) in ordered_points.iter().zip(ordered_points.iter().skip(1)) {
+        for t in 0..100 {
+            let (x1, x2, y1, y2) = (*x1 as f32, *x2 as f32, *y1 as f32, *y2 as f32);
+            let t = t as f32 / 100.0;
+            let new_x = (x1*t + x2*(1.0-t)) as usize;
+            let new_y = (y1*t + y2*(1.0-t)) as usize;
+            set_pixel(map, new_x, new_y, Tile::Wall);
+        }
         
     }
 
 }
+
+
+fn draw_map(map: &Map, width: f32) {
+    for y in 0..map.height {
+        for x in 0..map.width {
+            let color = match get_pixel(map, x, y) {
+                Tile::Empty { is_start, is_finish } => WHITE,
+                Tile::Wall => BLACK,
+            };
+            draw_rectangle(width * x as f32, width * y as f32, width, width, color);
+        }
+    }
+    
+} 
