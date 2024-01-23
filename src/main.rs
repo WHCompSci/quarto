1f111dd2bada882286e5c786eb1fc2e01d5bac5c
whcompsci <whcompsci@warrenhills.org>
2024-01-23 10:06:47 -0500

    working on generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use macroquad::prelude::*;
 mod neural_net;
 
@@ -49,11 +50,20 @@ fn get_pixel(map: &Map,x: usize, y: usize) -> Tile {
     map.tiles[y * map.width + x]
 }
 
+
+
 fn generate_map(map: &mut Map) {
+    let C0 = vec4(-1., 3., -3., 1.) * 0.5;
+    let C1 = vec4(2., -5., 4., -1.) * 0.5;
+    let C2 = vec4(-1., 0., 1., 0.) * 0.5;
+    let C3 = vec4(0., 2., 0., 0.) * 0.5;
     //Step 1: Generate Random Points
     let margin = 10_usize;
     let N = 4;
-    let mut random_points: Vec<(usize, usize)> = vec![];
+    let mut random_points: Vec<Vec2> = vec![];
+    let system_time = SystemTime::now();
+    rand::srand(system_time.duration_since(UNIX_EPOCH)
+    .expect("Time went backwards").as_millis() as u64);
     for quad_x in 0..N {
         for quad_y in 0..N {
             let x = rand::gen_range(
@@ -64,41 +74,32 @@ fn generate_map(map: &mut Map) {
                 margin + quad_y * (map.height - 2 * margin) / N,
                 margin + (quad_y + 1) * (map.height - 2 * margin) / N,
             );
-            random_points.push((x, y));
+            random_points.push(Vec2 {x: x as f32, y: y as f32});
         }
     }
     let mut visited = vec![false; random_points.len()];
-    let mut ordered_points: Vec<(usize, usize)> = vec![];
-    let (mut curr_x, mut curr_y) = random_points[0];
+    let mut ordered_points: Vec<Vec2> = vec![];
+    let mut curr = random_points[0];
     visited[0] = true; //set the first point to visited
-    ordered_points.push((curr_x, curr_y));
+    ordered_points.push(curr);
     for _ in 0..N*N {
         //do a linear search through all the remaining points to find the shortest distance
         let (mut shortest, mut shortest_idx) = (f32::INFINITY, 0);
-        for (i, (next_x, next_y)) in random_points.iter().enumerate() {
+        for (i, next) in random_points.iter().enumerate() {
             if visited[i] {continue;}
-            let (dx, dy) = (curr_x as f32 - *next_x as f32, curr_y as f32 - *next_y as f32);
-            let dist = (dx*dx + dy*dy).sqrt();
+            let dist = curr.distance(*next);
             if dist < shortest {
                 shortest = dist;
                 shortest_idx = i;
             }
         }
         ordered_points.push(random_points[shortest_idx]);
-        (curr_x, curr_y) = random_points[shortest_idx];
+        curr = random_points[shortest_idx];
         visited[shortest_idx] = true;
     }
     //testing set all points in ordered points to walls
-    for ((x1, y1),(x2, y2)) in ordered_points.iter().zip(ordered_points.iter().skip(1)) {
-        for t in 0..100 {
-            let (x1, x2, y1, y2) = (*x1 as f32, *x2 as f32, *y1 as f32, *y2 as f32);
-            let t = t as f32 / 100.0;
-            let new_x = (x1*t + x2*(1.0-t)) as usize;
-            let new_y = (y1*t + y2*(1.0-t)) as usize;
-            set_pixel(map, new_x, new_y, Tile::Wall);
-        }
-        
-    }
+    // for(let i = 0; i < )
+    //TODO http://alvyray.com/Memos/CG/Pixar/spline77.pdf page 3
 
 }
 
